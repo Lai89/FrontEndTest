ed62fc2b6ce2249645859805aaa195c98c9279cf
laia <santos.montorio@gmail.com>
2022-11-15 14:59:21 +0100

    Update test collection and test case description

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ This is an exercise to test the website (<https://www.demoblaze.com>).
 
 ## How To
 
-To execute the tests, you must have installed Selenium IDE. Tests are executed from the IDE; in this case, we used IntelliJ by IDEA.
+To execute the tests, you must have installed Selenium Driver for the navigator you are going to use. 
+Tests are executed from the IDE; in this case, we used IntelliJ by IDEA.
 
 ## Observations
 
-- It is very useful that web elements can be identified with the id, instead of some element more general.
+- It is the first time I create automation tests with Selenium and Java.
+- It is very useful that web elements can be identified with a unique id, instead of "link text" or "xpath". 
 - Checking validations, I considered it was an E2E case, and that is the reason why all assertions are found inside the same text.
+- Variables could be saved in the before section, but I prefer to build tests first with all the variables public and then, when needed, move them to the desired space. It would be an improvement.
+- The assertions when getting to cart and placing order have been commented because cannot be validated at the moment. It would be another improvement.
+- In order to log Id and Purchase Amount would need to break the text block of the element so they can be logged separately. That is why it is commented.
 
 ## Next Steps
 
 - Try to assign a unique id for each of the web elements, so we can find them easily. At the moment, some elements have the same id.
-- Create a separate file to map all the elements and avoid unnecessary repetition.
+- Create a separate file for each webpage to test in order to map all the elements and avoid unnecessary repetition.
 - We can do the same with the config.
+- We can improve the actions with the delete button in the cart, so it gets more dynamic
--- src/test/java/FrontEndTestClass.java
+++ src/test/java/FrontEndTestClass.java
@@ -1,88 +1,183 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FrontEndTestClass {
+    //Set variables
     private  static WebDriver driver;
+
     public WebElement phoneButton;
     public WebElement monitorButton;
     public WebElement laptopButton;
     public WebElement sonyVaio;
-
     public WebElement dell;
     public WebElement addToCart;
+    public Alert alert;
+    String alertText;
+    public WebElement homePage;
     public WebElement cartButton;
     public WebElement totalPurchase;
-    public WebElement Assertion;
+    public WebElement deleteButton;
+    public WebElement placeOrder;
+    public WebElement nameInput;
+    public WebElement countryInput;
+    public WebElement cityInput;
+    public WebElement cardInput;
+    public WebElement monthInput;
+    public WebElement yearInput;
+    public WebElement purchaseButton;
+    public WebElement okButton;
 
+    /*
+        Scenario: Make a purchase in the web store
 
+        GIVEN a user that wants to navigate through the store and see phones, laptops and monitors
+        AND add 2 laptops to the cart
+        AND delete one of them
+	    WHEN the order is placed with all the user and payment data
+	    THEN I will obtain the information of the purchase, which includes the Id and the total amount of the purchase.
+    */
     @Before
     public void setUp(){
         System.setProperty("webDriver.chrome.driver", "C:\\Users\\santo\\OneDrive\\Programacion\\DemoBlazeTest\\drivers\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
-        //driver.manage().window().maximize();
         driver.get("https://www.demoblaze.com/index.html");
     }
     @Test
     public void CustomerNavigation() throws InterruptedException {
-        //Permite ver la acción en el navegador emulado
+        //Add extra time so the action shows on emulated navigator
         Thread.sleep(2000);
-        //busca el elemento en la página
-        phoneButton = driver.findElement(By.id("itemc"));
+
+        //Find and click on "Phones"
+        phoneButton = driver.findElement(By.linkText("Phones"));
         phoneButton.click();
         Thread.sleep(2000);
-        monitorButton = driver.findElement(By.linkText("/html/body/div[5]/div/div[1]/div/a[4]"));
+
+        //Find and click on "Monitors"
+        monitorButton = driver.findElement(By.linkText("Monitors"));
+        monitorButton.click();
+        Thread.sleep(2000);
+
+        //Find and click on "Laptops"
+        laptopButton = driver.findElement(By.linkText("Laptops"));
+        laptopButton.click();
+        Thread.sleep(2000);
+
+        //Find and select "Sony Vaio i5"
+        sonyVaio = driver.findElement(By.linkText("Sony vaio i5"));
+        sonyVaio.click();
+        Thread.sleep(2000);
+
+        //Add "Sony vaio i5" to cart
+        addToCart = driver.findElement(By.linkText("Add to cart"));
+        addToCart.click();
+        Thread.sleep(2000);
+
+        //Get alert text
+        alert = driver.switchTo().alert();
+        alertText = driver.switchTo().alert().getText();
+        assertEquals("Product added", alertText);
+        driver.switchTo().alert().accept();
+
+        //Back to Home>Laptops
+        homePage = driver.findElement(By.cssSelector(".active > .nav-link"));
+        homePage.click();
+        Thread.sleep(2000);
+
+        laptopButton = driver.findElement(By.linkText("Laptops"));
+        laptopButton.click();
+        Thread.sleep(2000);
+
+        //Find and select "Dell i7 8gb"
+        dell = driver.findElement(By.linkText("Dell i7 8gb"));
+        dell.click();
+        Thread.sleep(2000);
+
+        //Add "Dell i7 8gb" to cart
+        addToCart = driver.findElement(By.linkText("Add to cart"));
+        addToCart.click();
+        Thread.sleep(2000);
+
+        //Get alert text
+        alert = driver.switchTo().alert();
+        assertEquals("Product added", alertText);
+        driver.switchTo().alert().accept();
+
+        //Go to Cart
+        cartButton = driver.findElement(By.id("cartur"));
+        cartButton.click();
         Thread.sleep(7000);
-//        laptopButton = driver.findElement(By.linkText("/html/body/div[5]/div/div[1]/div/a[3]"));
-//        Thread.sleep(7000);
-//        monitorButton = driver.findElement(By.linkText("/html/body/div[5]/div/div[1]/div/a[4]"));
-//        Thread.sleep(7000);
-//        sonyVaio = driver.findElement(By.linkText("Sony vaio i5"));
-//        Thread.sleep(7000);
-//        addToCart = driver.findElement(By.linkText("linkText=Add to cart"));
-//        Thread.sleep(7000);
-//        //www.demoblaze.com says
-//        assertEquals("Product Added", driver.getTitle());
-//
-//        dell = driver.findElement(By.linkText("Dell i7 8gb"));
-//        Thread.sleep(7000);
-//        addToCart = driver.findElement(By.linkText("Dell i7 8gb"));
-//        Thread.sleep(7000);
-//        //www.demoblaze.com says
-//        assertEquals("Product Added", driver.getTitle());
-//
-//        cartButton = driver.findElement(By.id("Cartur"));
-//        totalPurchase = driver.findElement(By.className("panel-title"));
-//        String totalAmount = totalPurchase.getText();
-//
-//        assertEquals("1490", totalAmount);
 
-        //Buscar elemento en la página
-        //WebElement searchbox = driver.findElement(By.name(""));
-        //Limpiar texto
-        //searchbox.clear();
-        //escribir en el navegador
-        //searchbox.sendKeys();
-        //enviar acción
-        //searchbox.submit();
+        //Assertions
+        //totalPurchase = driver.findElement(By.id("totalp"));
+        //String totalAmount = totalPurchase.getText();
+        //assertEquals("1490", totalPurchase);
 
-        //tiempo de espera
-        //driver.manage().timeouts().getImplicitWaitTimeout(10, TimeUnit.SECONDS);
+        //Delete "Dell i7 8gb"
+        deleteButton = driver.findElement(By.xpath("//*[@id=\"tbodyid\"]/tr[1]/td[4]/a"));
+        deleteButton.click();
+        Thread.sleep(4000);
 
-        //assertEquals("titulo página", driver.getTitle());
+        //Place Order
+        placeOrder = driver.findElement(By.xpath("//button[contains(.,'Place Order')]"));
+        placeOrder.click();
+        Thread.sleep(2000);
 
+        //Assertions
+        //String totalOrder = driver.findElement(By.id("totalm")).getText();
+        //assertEquals("Total: 790", totalOrder);
+
+        //Fill order data
+        //Name data
+        nameInput = driver.findElement(By.id("name"));
+        nameInput.sendKeys("Laia Santos");
+
+        //Country data
+        countryInput = driver.findElement(By.id("country"));
+        countryInput.sendKeys("Spain");
+
+        //City data
+        cityInput = driver.findElement(By.id("city"));
+        cityInput.sendKeys("Zaragoza");
+
+        //Card data
+        cardInput = driver.findElement(By.id("card"));
+        monthInput = driver.findElement(By.id("month"));
+        yearInput = driver.findElement(By.id("year"));
+        cardInput.sendKeys("370000000000002");
+        monthInput.sendKeys("03");
+        yearInput.sendKeys("2030");
+
+        //Click on Purchase
+        purchaseButton = driver.findElement(By.cssSelector("#orderModal .btn-primary"));
+        purchaseButton.click();
+
+        /*
+        Log Id and Purchase
+        String purchaseId = driver.findElement(By.xpath("/html/body/div[10]/p/text()[1]")).getText();
+        System.out.println(purchaseId);
+        String purchaseAmount = driver.findElement(By.xpath("/html/body/div[10]/p/text()[2]")).getText();
+        System.out.println(purchaseAmount);
+        assertEquals("Amount: 790 USD", purchaseAmount);
+        */
+
+        //Click on OK
+        okButton = driver.findElement(By.xpath("//button[contains(.,'OK')]"));
+        //button[contains(.,'OK')]
     }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
+        @After
+        public void tearDown(){
+            driver.quit();
+        }
 
 }
